15366523196f221fced406a967b060db753a9e7b
gorbanevi <gorbanev_i@mail.ru>
2021-07-30 17:23:21 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function calculateCashback(specialCategoryPurchases, otherCategoryPurchases) {
+    const specialCategoryPercent=0.03;
+    const otherCategoryPercent=0.01;
+    const specialCategoryCashback=specialCategoryPurchases*specialCategoryPercent;
+    console.log(specialCategoryCashback);
+    const otherCategoryCashback=otherCategoryPurchases*otherCategoryPercent;
+    console.log(otherCategoryCashback);
+    let totalCashback=specialCategoryCashback+otherCategoryCashback;
+    const limit =15000;
+
+    return {
+        specialCategoryCashback,
+        otherCategoryCashback,
+        totalCashback: totalCashback > limit ? limit : totalCashback,
+    };
+}
+const cashback= calculateCashback(250000,700000);
+console.log(cashback);
+// calculateCashback();
